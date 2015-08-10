111bf1d62e3d177417e10f282233fef66f3a44d9
Al Nemec <al.nemec@gmail.com>
2015-08-09 22:42:21 -0500

    Added some starting layouts for displays

---
+++ template-parts/content-single_display.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Template part for displaying single posts.
+ *
+ * @package vmmm
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+
+	<div class="entry-content">
+		
+		<div class="visuals">
+			<script src="<?php echo get_template_directory_uri(); ?>/jsmol/JSmol.min.js"></script>
+
+			<script type="text/javascript">
+var themePath = "<?php echo get_template_directory_uri(); ?>";
+var uploadedFile = "<?php the_field('mol_file'); ?>";
+ 
+Jmol._isAsync = false;
+
+// last update 2/18/2014 2:10:06 PM
+
+var jmolApplet0; // set up in HTML table, below
+
+// logic is set by indicating order of USE -- default is HTML5 for this test page, though
+
+var s = document.location.search;
+
+// Developers: The _debugCode flag is checked in j2s/core/core.z.js, 
+// and, if TRUE, skips loading the core methods, forcing those
+// to be read from their individual directories. Set this
+// true if you want to do some code debugging by inserting
+// System.out.println, document.title, or alert commands
+// anywhere in the Java or Jmol code.
+
+Jmol._debugCode = (s.indexOf("debugcode") >= 0);
+
+jmol_isReady = function(applet) {
+	document.title = (applet._id + " - Jmol " + Jmol.___JmolVersion)
+	Jmol._getElement(applet, "appletdiv").style.border="1px solid blue"
+}		
+
+var Info = {
+	width: '100%',
+	height: 600,
+	debug: false,
+	color: "0x112233",
+	addSelectionOptions: true,
+	use: "HTML5",   // JAVA HTML5 WEBGL are all options
+	j2sPath: themePath+"/jsmol/j2s", // this needs to point to where the j2s directory is.
+	jarPath: themePath+"/jsmol/java",// this needs to point to where the java directory is.
+	jarFile: "JmolAppletSigned.jar",
+	isSigned: true,
+	script: "set antialiasDisplay;load "+uploadedFile+"",
+	serverURL: "http://chemapps.stolaf.edu/jmol/jsmol/php/jsmol.php",
+	readyFunction: jmol_isReady,
+	disableJ2SLoadMonitor: true,
+  disableInitialConsole: true,
+  allowJavaScript: true
+	//defaultModel: "$dopamine",
+	//console: "none", // default will be jmolApplet0_infodiv, but you can designate another div here or "none"
+}
+
+$(document).ready(function() {
+  $("#appdiv").html(Jmol.getAppletHtml("jmolApplet0", Info))
+})
+var lastPrompt=0;
+</script>
+
+<div id="appdiv"></div>
+	
+		</div>
+		
+		<div class="contentColumn">
+		<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+			<?php vmmm_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+		<?php the_content(); ?>
+		
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'vmmm' ),
+				'after'  => '</div>',
+			) );
+		?>
+		</div>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php vmmm_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
