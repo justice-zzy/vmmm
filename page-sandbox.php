022edf7064bc997f0284fe75d96801fca1fb1dbd
Al Nemec <al.nemec@gmail.com>
2016-11-14 18:48:23 -0600

    updated markup for highlighted content links

---
+++ page-sandbox.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Script Sandbox
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package vmmm
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'single_display_sandbox' ); ?>
+
+				<?php the_post_navigation(); ?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
