19102cfa65b191c2441f190a3ee62453d07bdb8a
Al Nemec <al.nemec@gmail.com>
2016-11-22 15:38:19 -0600

    fixed display labeling

--- template-parts/content-single_display.php
+++ template-parts/content-single_display.php
@@ -321,7 +321,7 @@ var lastPrompt=0;
 				<?php while ( have_rows('controls_after_content') ) : the_row(); ?>
 				
 					<?php if( get_row_layout() == 'add_formatted_control' ) { ?>
-							<a href="javascript:Jmol.script(jmolApplet0, 'script <?php the_sub_field('control_action'); ?>')" class="<?php the_sub_field('control_label'); ?> highlightingLink"><?php the_sub_field('control_label'); ?></a>
+							<a href="javascript:Jmol.script(jmolApplet0, 'script <?php the_sub_field('control_action'); ?>')" class="highlightingLink"><?php the_sub_field('control_label'); ?></a>
 							<div class="controlDescription"><?php the_sub_field('control_description'); ?></div>
 					<?php } else { ?>
 							<?php the_sub_field('custom_html'); ?>
