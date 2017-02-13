4f255aad3812cfc93b17092322ac0d597d623b1a
Al Nemec <al.nemec@gmail.com>
2017-02-13 10:27:56 -0600

    changing jquery include to force https

--- functions.php
+++ functions.php
@@ -165,7 +165,7 @@ add_action( 'after_setup_theme', 'vmmm_setup' );
 if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
 function my_jquery_enqueue() {
    wp_deregister_script('jquery');
-   wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js", false, null);
+   wp_register_script('jquery', "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js", false, null);
    wp_enqueue_script('jquery');
 }
 
